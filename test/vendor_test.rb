e60f31546bdc24915afff3b2e1d246e021a23492
Chandler Hulstrom <chulstro11@gmail.com>
2020-06-10 09:28:17 -0600

    Added stocking functionality to test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -17,10 +17,17 @@ class VendorTest < Minitest::Test
   end
 
   def test_it_can_add_inventory
-    expected = {@item1 => 30}
+    expected = {
+      @item1 => 55,
+      @item2 => 12
+    }
 
     assert_equal 0, @vendor.check_stock(@item1)
     @vendor.stock(@item1, 30)
-    assert_equal expected, @vendor.check_stock(@item1)
+    assert_equal 30, @vendor.check_stock(@item1)
+    @vendor.stock(@item1, 25)
+    assert_equal 55, @vendor.check_stock(@item1)
+    @vendor.stock(@item2, 12)
+    assert_equal expected, @vendor.inventory
   end
 end
