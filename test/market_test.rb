130f4bacf0aaa8d04ed265464d44a01ded9b5270
Chandler Hulstrom <chulstro11@gmail.com>
2020-06-10 10:03:28 -0600

    Wrote test to check a markets total item inventory

--- test/market_test.rb
+++ test/market_test.rb
@@ -51,4 +51,22 @@ class MarketTest < Minitest::Test
     assert_equal 345, @vendor2.potential_revenue
     assert_equal 48.75, @vendor3.potential_revenue
   end
+
+  def test_the_total_inventory
+    expected = {
+
+    }
+
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal expected, @market.total_inventory
+  end
 end
