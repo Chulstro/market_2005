dd42ab179b4899ca1072ca2439624384873149be
Chandler Hulstrom <chulstro11@gmail.com>
2020-06-10 09:20:59 -0600

    Initial test for checking stock written

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -15,4 +15,12 @@ class VendorTest < Minitest::Test
     assert_equal "Rocky Mountain Fresh", @vendor.name
     assert_equal expected, @vendor.inventory
   end
+
+  def test_it_can_add_inventory
+    expected = {@item1 => 30}
+
+    assert_equal 0, @vendor.check_stock(@item1)
+    @vendor.stock(@item1, 30)
+    assert_equal expected, @vendor.check_stock(@item1)
+  end
 end
